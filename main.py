7c1bd9e43759e0e1f18354a32a6d796297576517
eric <eggit.ejs@gmail.com>
2024-04-14 22:29:27 +0100

    first

---
+++ main.py
@@ -0,0 +1,34 @@
+import yfinance as yf
+import pandas as pd
+
+
+# # get all stock info from yfinance
+# spy = yf.Ticker("^GSPC")
+# historydf = spy.history(interval="1d", period="max")
+# # print(list(historydf.axes))
+# historydf.index = historydf.index.tz_localize(None)
+# print(historydf)
+# # save data to excel sheet
+# historydf.to_excel("stockData.xlsx")
+
+
+historydf = pd.read_excel("stockData.xlsx", index_col="Date")
+
+historydf['HighLow Height abs'] = abs(historydf["High"] - historydf["Low"])
+
+
+maPeriodCount = 200
+
+historydf[f"HighLow Height MA({maPeriodCount})"] = historydf.rolling(maPeriodCount)["HighLow Height abs"].mean()
+
+historydf[f"HighLow Height MAX({maPeriodCount})"] = historydf.rolling(maPeriodCount)["HighLow Height abs"].max()
+
+historydf['OpenClose Height abs'] = abs(historydf["Close"] - historydf["Open"])
+
+historydf[f"OpenClose Height MA({maPeriodCount})"] = historydf.rolling(maPeriodCount)["OpenClose Height abs"].mean()
+
+historydf[f"OpenClose Height MAX({maPeriodCount})"] = historydf.rolling(maPeriodCount)["OpenClose Height abs"].max()
+
+print(historydf)
+
+historydf.plot.line()
